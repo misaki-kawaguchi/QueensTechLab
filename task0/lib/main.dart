89835f0168f76ec780f40a71ab7938f9f4dd92b3
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-03-08 20:13:13 +0900

    カウント表示されるテキストの色を赤に変える

--- task0/lib/main.dart
+++ task0/lib/main.dart
@@ -52,7 +52,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headline4?.copyWith(
+                    color: Colors.red,
+                  ),
             ),
           ],
         ),
