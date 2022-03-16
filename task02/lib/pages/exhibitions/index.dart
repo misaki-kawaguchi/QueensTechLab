1e024fc53be142fa41046d94242bb4a7646952ca
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-03-17 02:42:46 +0900

    GridViewを追加

--- task02/lib/pages/exhibitions/index.dart
+++ task02/lib/pages/exhibitions/index.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:task02/widgets/common_drawer.dart';
 import 'package:task02/widgets/common_app_bar.dart';
+import 'dart:math';
 
 class ExhibitionsPage extends StatelessWidget {
   const ExhibitionsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CommonAppBar(),
-      endDrawer: AppBarDrawer(),
-      body: Text('Exhibitions & Events'),
+    return Scaffold(
+      appBar: const CommonAppBar(),
+      endDrawer: const AppBarDrawer(),
+      body: GridView.count(
+        padding: const EdgeInsets.all(10),
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.95,
+        shrinkWrap: true,
+        children: List.generate(50, (index) {
+          var imgNumber = Random().nextInt(6) + 1;
+          var assetsImage = "images/exhibition" + imgNumber.toString() + ".jpg";
+
+          return Column(children: <Widget>[
+            Image.asset(
+              assetsImage,
+              fit: BoxFit.cover,
+              height: 150.0,
+              width: double.infinity,
+            ),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.only(top: 10),
+              child: const Text(
+                'sample',
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ]);
+        }),
+      ),
     );
   }
 }
