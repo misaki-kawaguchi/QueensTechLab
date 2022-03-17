2fa2078f0a81b9337a4bf1f228dfe8a8f83e5d72
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-03-17 15:03:53 +0900

    routesを修正

--- task02/lib/routes.dart
+++ task02/lib/routes.dart
@@ -1,12 +1,12 @@
 import 'package:task02/pages/login.dart';
-import 'pages/index.dart';
+import 'package:task02/pages/index.dart';
 import 'package:task02/pages/exhibitions/index.dart';
 import 'package:task02/pages/plan/index.dart';
 
 const loginPage ='/';
 const indexPage = '/index';
-const exhibitionsPage = 'exhibitions/index';
-const planYourVisitPage = 'plan/index';
+const exhibitionsPage = '/exhibitions/index';
+const planYourVisitPage = '/plan/index';
 
 final routes = {
   loginPage: (context) => const LoginPage(),
